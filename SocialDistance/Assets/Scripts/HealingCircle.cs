b5e939e47a31b7bb9ed0cd280cf80bad63443bea
KylevdBoom <kyle.van.den.boom@accenture.com>
2020-04-01 12:33:55 +0200

    Added a followcamera script to circle

---
+++ SocialDistance/Assets/Scripts/HealingCircle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingCircle : MonoBehaviour
+{
+    [SerializeField] GameObject m_cameraobj;
+    
+    void Update()
+    {
+        transform.position = new Vector3(m_cameraobj.transform.position.x, m_cameraobj.transform.position.y - 16, m_cameraobj.transform.position.z); 
+    }
+}
