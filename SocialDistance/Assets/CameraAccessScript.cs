8179a245740e1e0080520305634c083c094cbf87
Bas du Pré <me@basdupre.com>
2020-04-03 13:05:35 +0200

    main menu

---
+++ SocialDistance/Assets/CameraAccessScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAccessScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Nope()
+    {
+
+    }
+
+    public void OK()
+    {
+
+    }
+}
