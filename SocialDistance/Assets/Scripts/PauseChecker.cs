22f32f8e1c072f15c83951fbe4fbbce882050917
KylevdBoom <kyle.van.den.boom@accenture.com>
2020-04-23 09:38:07 +0200

    Pausechecker updateed

--- SocialDistance/Assets/Scripts/PauseChecker.cs
+++ SocialDistance/Assets/Scripts/PauseChecker.cs
@@ -7,18 +7,20 @@ public class PauseChecker : MonoBehaviour
     bool isPaused = false;
 
     [SerializeField] GameObject m_pausedobj;
+    [SerializeField] GameObject m_pausedfritz;
     
     void Update()
     {
-        Debug.Log(isPaused);
-
         if(!isPaused)
         {
             m_pausedobj.SetActive(true);
+            m_pausedfritz.SetActive(true);
         }
         else
         {
             m_pausedobj.SetActive(false);
+            m_pausedfritz.SetActive(false);
+
         }
     }
 
